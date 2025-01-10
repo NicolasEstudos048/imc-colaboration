a71c8caa6e3846b4a85e17029733efb82adc804d
NicolasEstudos048 <estudosnicolas2@gmail.com>
2025-01-10 20:22:45 -0300

    Excluindo arquivo padrão App.java, Adicionando a classe main e a classe de variáveis

---
+++ src/Variaveis.java
@@ -0,0 +1,20 @@
+public class Variaveis {
+    private double peso;
+    private double altura;
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+}
