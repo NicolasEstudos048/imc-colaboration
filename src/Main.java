364ab89428a5e32a2018e9eecafce80b4fa5b038
NicolasEstudos048 <estudosnicolas2@gmail.com>
2025-01-10 21:46:22 -0300

    Criando interface e exibindo resultado

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        Variaveis calc = new Variaveis();
+
+        System.out.print("Qual o seu peso? ");
+        calc.setPeso(input.nextDouble());
+
+        System.out.print("Qual a sua altura? ");
+        calc.setAltura(input.nextDouble());
+
+        System.out.println("Peso: " + calc.getPeso());
+
+        System.out.println("Altura: " + calc.getAltura());
+
+        System.out.println("Seu imc é: " + calc.calcularImc());
     }
 }
